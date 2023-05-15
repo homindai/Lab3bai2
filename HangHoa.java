4896d788b8adf1b69870e5e0999b4160ec3c5069
homindai <129465307+homindai@users.noreply.github.com>
2023-05-15 16:50:13 +0700

    Create HangHoa.java

---
+++ HangHoa.java
@@ -0,0 +1,20 @@
+package Lab3bai2;
+
+
+abstract class HangHoa {
+    protected String maHang;
+    protected String tenHang;
+    protected int soLuongTon;
+    protected double donGia;
+
+    public HangHoa(String maHang, String tenHang, int soLuongTon, double donGia) {
+        this.maHang = maHang;
+        this.tenHang = tenHang;
+        this.soLuongTon = soLuongTon;
+        this.donGia = donGia;
+    }
+
+    public abstract double tinhTienVAT();
+
+    public abstract void danhGia();
+}
